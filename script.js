27d62182d9bc427d45fefb18ab900e156952dbd3
aryanl10yd <66977296+aryanl10yd@users.noreply.github.com>
2023-05-08 00:06:12 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-    $('.icon1').hover(function() {
-      $('.text').animate({
-        "marginLeft": "20"
-      }, 500);
-    }, function() {
-      $('.text').animate({
-        "marginLeft": "150"
-      }, 500);
-    });
