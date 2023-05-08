c12984c9542ed48abc28551d374cd775c4eec1f7
aryanl10yd <66977296+aryanl10yd@users.noreply.github.com>
2023-05-07 23:52:49 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+    $('.icon1').hover(function() {
+      $('.text').animate({
+        "marginLeft": "20"
+      }, 500);
+    }, function() {
+      $('.text').animate({
+        "marginLeft": "150"
+      }, 500);
+    });
